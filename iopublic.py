1980c3b07f47e3dc1ad2f2523356a668be149876
Lennart Landsmeer <lennart@landsmeer.email>
2022-02-22 17:23:54 +0100

    local field potentials

--- iopublic.py
+++ iopublic.py
@@ -93,9 +93,10 @@ class TunedIOModel(arbor.recipe):
         return [
                 # just vsoma
                 arbor.cable_probe_membrane_voltage('"root"'),
-                # next 2 are needed for lfpykit
+                # next 3 are needed for lfpykit
                 arbor.cable_probe_membrane_voltage_cell(),
-                arbor.cable_probe_total_current_cell()
+                arbor.cable_probe_total_current_cell(),
+                arbor.cable_probe_stimulus_current_cell(),
                 ]
     def global_properties(self, kind): return self.props
 
@@ -207,6 +208,7 @@ def make_space_filling_neuron(neuron, mod=(), gjs=(), noise=(), just_tree=False)
     mod = dict(mod)
     tree = arbor.segment_tree()
 
+    '''
     def cable(*, length, radius, parent, tag):
         if isinstance(radius, (float, int)):
             radius = [radius, radius]
@@ -215,9 +217,30 @@ def make_space_filling_neuron(neuron, mod=(), gjs=(), noise=(), just_tree=False)
             arbor.mpoint(x=0, y=0, z=0, radius=radius[0]),
             arbor.mpoint(x=length, y=0, z=0, radius=radius[1]),
             tag=tag)
+    '''
 
-    s = cable(length=12, radius=6, parent=arbor.mnpos, tag=1)
-    a = cable(length=20, radius=[2.5, 1.5], parent=s, tag=2)
+    def cable3d(*, a, b, radius, parent, tag):
+        if isinstance(radius, (float, int)):
+            radius = [radius, radius]
+        return tree.append(
+            parent,
+            arbor.mpoint(x=a[0], y=a[1], z=a[2], radius=radius[0]),
+            arbor.mpoint(x=b[0], y=b[1], z=b[2], radius=radius[1]),
+            tag=tag)
+
+    # s = cable(length=12, radius=6, parent=arbor.mnpos, tag=1)
+    # a = cable(length=20, radius=[2.5, 1.5], parent=s, tag=2)
+    # sample random normal for soma and axon orientation
+    # not for simulation but for lfp calculation
+    # shouldnt matter too much, just as long we don't introduce a bias
+    # by always orienting it in one way
+    normal = np.random.randn(3)
+    normal /= np.linalg.norm(normal, axis=0)
+    soma_pos = np.array([neuron.x, neuron.y, neuron.z])
+    half_soma_end =  6 * normal
+    axon_end = 20 * normal
+    s = cable3d(a=soma_pos-half_soma_end, b=soma_pos+half_soma_end, radius=6, parent=arbor.mnpos, tag=1)
+    a = cable3d(a=soma_pos-half_soma_end, b=soma_pos-half_soma_end-axon_end, radius=[2.5, 1.5], parent=s, tag=2)
 
     segments = list(sorted(neuron.tree, key=lambda seg:seg.seg_id))
     seg_to_cable = {0: s}
@@ -228,8 +251,11 @@ def make_space_filling_neuron(neuron, mod=(), gjs=(), noise=(), just_tree=False)
             continue # soma
         prev_seg = segments[seg.parent]
         prev_cable_id = seg_to_cable[seg.parent]
-        length = math.sqrt((prev_seg.x-seg.x)**2 + (prev_seg.y-seg.y)**2 + (prev_seg.z-seg.z)**2)
-        cable_id = cable(length=length, radius=1, parent=prev_cable_id, tag=3)
+        # length = math.sqrt((prev_seg.x-seg.x)**2 + (prev_seg.y-seg.y)**2 + (prev_seg.z-seg.z)**2)
+        #cable_id = cable(length=length, radius=1, parent=prev_cable_id, tag=3)
+        a = np.array([prev_seg.x, prev_seg.y, prev_seg.z])
+        b = np.array([seg.x, seg.y, seg.z])
+        cable_id = cable3d(a=a, b=b, radius=1, parent=prev_cable_id, tag=3)
         seg_to_cable[i] = cable_id
         for gj in seg.gj:
             labels[f'gj{gj}'] = f'(on-components 1.0 (segment {cable_id}))'
